e3e46d959e38d59a1e91ef88ceef448cf3f9e949
Karoly Pakozdi <Karoly.Pakozdi@logmein.com>
2020-08-24 20:07:51 +0200

    Fixing timestamps. (+4 squashed commits) Squashed commits: [b51d096] Fixing timestamps. [a316013] Fixing timestamps. [c9cbcc6] Fixing timestamps. [eb670a6] Fixing timestamps.

--- migrations/20200824195349_todo.js
+++ migrations/20200824195349_todo.js
@@ -4,7 +4,7 @@ exports.up = function (knex) {
         table.string('title').notNullable();
         table.text('description');
         table.boolean('priority').notNullable().defaultTo(false);
-        table.timestamps(false, true);
+        table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
     });
 };
 
